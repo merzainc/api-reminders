2f34970cfc6a459161073c7a38189ac35db05824
merzainc <believe@noclon.co.zw>
2023-02-08 11:34:50 +0200

    Add cors middleware into app

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import reminders from './routers/reminders';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/reminders', reminders);
 
 const port = process.env.PORT || 5000;
